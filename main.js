2ded01b4d815316c57082a8b601c8af161cf1ade
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2021-06-18 14:36:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
-var copy = document.getElementById('copy');
-
-
 $(function() {
   $('#file').change(function(e) {
-    alert('change');
     var file = e.target.files[0];
     var reader = new FileReader();
     reader.onload = function() {
-      alert('loaded');
       var data = reader.result
       nbt.parse(data, function(error, data) {
         if (error) {
           throw error;
         }
-        var outputJson = JSON.stringify(data));
-        document.getElementById('textarea').value = outputJson;
-
+        var textVal = JSON.stringify(data, null, 2);
+        document.getElementById('textarea').value = textVal;
       });
     }
+
     reader.readAsArrayBuffer(file);
   });
 });
-function copyText() {
-  var textarea = document
-  // テキストエリアの値を選択
-  copyFrom.select();
-  // コピーコマンド発行
-  var retVal = document.execCommand('copy');
+
+function copy() {
+
 }
