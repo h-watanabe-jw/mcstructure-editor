e05f2afa4d3abd655f724edc9a8767fbdd54810d
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2021-06-25 23:05:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 var textarea = document.getElementById('textarea');
 var copy = document.getElementById('copy');
+var inputfile = document.getElementById('file');
 
 alert('js loaded');
 
+inputfile.addEventListener('change', function(e) {
+alert('change-v');
+  var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function() {
+      var data = reader.result
+      nbt.parse(data, function(error, data) {
+        if (error) {
+          throw error;
+        }
+        var textVal = JSON.stringify(data, null, 2);
+        document.getElementById('textarea').value = textVal;
+      });
+    }
+}
+                           
 $(function() {
   $('#file').change(function(e) {
     alert('change');
